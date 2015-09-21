f16f6be48879f55a17a4726a5a96ae9e97737e81
Seba Gamboa <me@sagmor.com>
2015-09-21 15:17:51 -0300

    Updated Array docs

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,10 @@
 var searchData=
 [
+  ['mrb_5fary_5fnew',['mrb_ary_new',['../group__mruby__array.html#ga8206dab92a483224cc17b62805b0ceac',1,'array.h']]],
+  ['mrb_5fary_5fpop',['mrb_ary_pop',['../group__mruby__array.html#ga004726adf37de6097ed4078c1e02d69c',1,'array.h']]],
+  ['mrb_5fary_5fpush',['mrb_ary_push',['../group__mruby__array.html#gacbc68e708c02f5812532ed00bba09c36',1,'array.h']]],
+  ['mrb_5fary_5fref',['mrb_ary_ref',['../group__mruby__array.html#ga979f5b26ee10b826a35e3bc46a1eff7d',1,'array.h']]],
+  ['mrb_5fary_5fset',['mrb_ary_set',['../group__mruby__array.html#ga252404ad8cf4d5fa2f7834e1a0707ef1',1,'array.h']]],
   ['mrb_5fclass_5fnew_5finstance',['mrb_class_new_instance',['../group__mruby.html#ga05117cc9d150df3921cdade39a74d59e',1,'mruby.h']]],
   ['mrb_5fclose',['mrb_close',['../group__mruby.html#ga3162c54447ae9ba28ff910c77a647438',1,'mruby.h']]],
   ['mrb_5fdefault_5fallocf',['mrb_default_allocf',['../group__mruby.html#gabe3711611530128d5ac6b2577d67beb5',1,'mruby.h']]],
