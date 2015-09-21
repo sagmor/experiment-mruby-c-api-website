8159052e1cda3c1ef0f57a9372867f61f629b068
Seba Gamboa <me@sagmor.com>
2015-09-21 12:08:34 -0300

    Updated mruby docs source

--- search/all_d.js
+++ search/all_d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['string_20class',['String class',['../group__mrb__string.html',1,'']]],
+  ['shared_20compiler_20macros',['Shared compiler macros',['../group__mruby__common.html',1,'']]],
   ['string_2eh',['string.h',['../string_8h.html',1,'']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,11 +1,15 @@
 var searchData=
 [
+  ['mrb_5fclass_5fnew_5finstance',['mrb_class_new_instance',['../group__mruby.html#ga05117cc9d150df3921cdade39a74d59e',1,'mruby.h']]],
   ['mrb_5fclose',['mrb_close',['../group__mruby.html#ga3162c54447ae9ba28ff910c77a647438',1,'mruby.h']]],
+  ['mrb_5fdefault_5fallocf',['mrb_default_allocf',['../group__mruby.html#gabe3711611530128d5ac6b2577d67beb5',1,'mruby.h']]],
   ['mrb_5fdefine_5fclass',['mrb_define_class',['../group__mruby.html#ga32269124fb30dbfd18130b6c20f40ccc',1,'mruby.h']]],
   ['mrb_5fdefine_5fmethod',['mrb_define_method',['../group__mruby.html#ga2033edc21be0fa5517b4ba8e4a67bf68',1,'mruby.h']]],
   ['mrb_5ffuncall',['mrb_funcall',['../group__mruby.html#gac2a27203156a0ce95239ca2ccc6979a6',1,'mruby.h']]],
   ['mrb_5fget_5fargs',['mrb_get_args',['../group__mruby.html#ga6659ff35e634812fff48bb6a7f66730e',1,'mruby.h']]],
+  ['mrb_5fobj_5fnew',['mrb_obj_new',['../group__mruby.html#gae8da10e0afdfd7d3da8ceead6cd4ac03',1,'mruby.h']]],
   ['mrb_5fopen',['mrb_open',['../group__mruby.html#gadbd82e1644b4653fc001cb46b919cc79',1,'mruby.h']]],
-  ['mrb_5fopen_5fallocf',['mrb_open_allocf',['../group__mruby.html#ga432a4972219d9459e5920f8a39f1b182',1,'mruby.h']]],
+  ['mrb_5fopen_5fallocf',['mrb_open_allocf',['../group__mruby.html#ga2eb00b692e5f3a317f213c42cbf8d1e9',1,'mruby.h']]],
+  ['mrb_5fopen_5fcore',['mrb_open_core',['../group__mruby.html#ga2e28b8e0d6c6fa6b94fdbe32d7d8bf11',1,'mruby.h']]],
   ['mrb_5fstr_5fnew_5fcstr',['mrb_str_new_cstr',['../group__mruby.html#gab86ff1ed3d1454d76c54aabbd6dff676',1,'mruby.h']]]
 ];
